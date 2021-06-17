7932f076f1b8a6dcc9e955773117059700778781
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-06-17 14:08:11 +0200

    standard --fix

--- tests/exportContent.test.js
+++ tests/exportContent.test.js
@@ -1,12 +1,11 @@
 const { expect } = require('@jest/globals')
 const pdfReader = require('../lib/exportContent')
+const pdfPath = './examplePdf.pdf'
 
-test("Check that output is object with correct properties", async () => {
-    let pdfPath = "./examplePdf.pdf"
-    const testObject = await pdfReader(pdfPath)
-    expect(typeof testObject).toBe("object")
-    expect(typeof testObject.metadata).toBe("object")
-    expect(Array.isArray(testObject.textContent)).toBe(true)
-    expect(typeof testObject.styles).toBe("object")
-    console.log(testObject)
+test('Check that output is object with correct properties', async () => {
+  const testObject = await pdfReader(pdfPath)
+  expect(typeof testObject).toBe('object')
+  expect(typeof testObject.metadata).toBe('object')
+  expect(Array.isArray(testObject.textContent)).toBe(true)
+  expect(typeof testObject.styles).toBe('object')
 })
