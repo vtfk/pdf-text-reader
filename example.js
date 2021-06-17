e4c8ba8ed179dbdb3400cf1ae87fc86632febe0a
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2021-06-17 12:38:56 +0200

    First commit

---
+++ example.js
@@ -0,0 +1,18 @@
+const pdfReader = require('.');
+
+(async () => {
+  const pdfPath = './examplePdf.pdf'
+  try {
+    const pdfData = await pdfReader(pdfPath)
+
+    // console.log(pdfData.metadata);
+
+    // console.log(pdfData.textContent);
+
+    // console.log(pdfData.styles)
+
+    console.log(pdfData)
+  } catch (error) {
+    console.log(error)
+  }
+})()
